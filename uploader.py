6c988d9cef8dffa4a4566a0227f3c299e2cbfc8d
Mehr Sachal <mehrsachal@gmail.com>
2022-12-07 13:24:10 +0500

    Added JSON exporter snippet.

--- uploader.py
+++ uploader.py
@@ -1,28 +1,45 @@
 import w3storage
 import json
+import os
+from os import listdir
+import pathlib
+import time
+import calendar
 
 
 w3 = w3storage.API(token='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDREQTVhNUREMGQxNjhiOGQzNjNCRDEzMjc5MDZERWE5NzRFQjU0MmUiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2NzAzOTQ0NjA1MjAsIm5hbWUiOiJ0bXNkZXNrdG9wIn0.DbezhIwSY8tNkH2hsHM_cYwf-pc3g5W8XxoPYb3VneA')
 cidlist = {}
 
+#temp code
 #some_uploads = w3.user_uploads(size=25)
 
 #ITERATE WITHIN FOLDER AND UPLOAD ALL FILES
 
-#ENCRYPT IMAGES
-cid = w3.post_upload((open('p5.jpg', 'rb')))
+current_dir = str(pathlib.Path().absolute()) #Current Directory 
+folder_dir = current_dir + '\data'
 
-#SAVE ALL FILES AS JSON STRING WITH FILE TYPES AND CID.
+for images in os.listdir(folder_dir):
 
+	# check if the image ends with png
+    if (images.endswith(".jpg")):
+        
+        imgpath = current_dir+'\data'+'\\' +images # jump to data folder and fetch image
+        cid = w3.post_upload((open(imgpath, 'rb'))) #upload to web3 storage
+        cidlist[images] = cid #write to dict
+        
 
-cidlist[filename] = cid #write to dict
-print(cidlist)
+#CONVERT TO JSON
 
-# Serializing json
 json_object = json.dumps(cidlist, indent=4)
 
-# Writing to file
-with open("timestampedname.json", "w") as outfile:
+#Get current timestamp
+gmt = time.gmtime()
+
+ts = str(calendar.timegm(gmt)) + '.json' #Convert timestamp to string and add relevent file extension
+
+
+#Write to json file with latest timestamp as file name in UNIX format
+with open(ts, "w") as outfile:
 	outfile.write(json_object)
 
 
