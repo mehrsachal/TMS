7156001abb68d595f199676f06fe64c749e35441
Mehr Sachal <mehrsachal@gmail.com>
2022-12-19 21:36:08 +0500

    change

--- wodm.py
+++ wodm.py
@@ -1,6 +1,6 @@
 import pyodm
 
-client = pyodm.Client("http://localhost:8000")
+client = pyodm.Client("http://localhost:8000", "root", "toor")
 
 project = client.projects.create(name="My Project", description="This is my project")
 print("Project created with ID:", project.id)
