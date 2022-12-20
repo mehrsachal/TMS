dda723dabc48a68a3b6fa7e4fce19a685ad61802
Mehr Sachal <mehrsachal@gmail.com>
2022-12-20 10:04:34 +0500

    upd

--- wodm.py
+++ wodm.py
@@ -1,6 +1,6 @@
 import os
 from pyodm import Node
-n = Node('localhost', 3000)
+n = Node('localhost', 8000)
 task = n.create_task(['image.png'], {'dsm': True})
 task.wait_for_completion()
 os.listdir(task.download_assets("results"))[0:2]
