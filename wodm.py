4a6f2847fd7e761ccd48945d34ffe5451311d03c
Mehr Sachal <mehrsachal@gmail.com>
2022-12-20 10:23:01 +0500

    upd2

--- wodm.py
+++ wodm.py
@@ -4,6 +4,4 @@ image_path = "image.png"
 # Load the project
 project = pyodm.Project.from_project_path("myproject")
 
-# Add an image to the project
-image_path = "myimage.jpg"
 project.add_image(image_path)
