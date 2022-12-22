717adb89a4fbff5c5d925114a234e0124e33eb50
Mehr Sachal <mehrsachal@gmail.com>
2022-12-22 09:42:44 +0500

    upd

--- wodm.py
+++ wodm.py
@@ -3,3 +3,4 @@ res = requests.post('http://localhost:8000/api/token-auth/',
                     data={'username': 'root',
                           'password': 'toor'}).json()
 token = res['token']
+print(token)
